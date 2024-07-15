0d5c55cea53e09b091c188e1126c2ab5e4cfbff0
Izi Kapampa <56266013+kingIzi@users.noreply.github.com>
2024-07-15 16:58:01 +0300

    added Branch & Country Setup

--- JICHANGEAPI/Controllers/CountryController.cs
+++ JICHANGEAPI/Controllers/CountryController.cs
@@ -15,8 +15,127 @@ namespace JichangeApi.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CountryController : ApiController
     {
+        [HttpPost]
+        public HttpResponseMessage GetCountries()
+        {
+            var country = new COUNTRY();
+            var result = country.GETcountries();
+            if (result != null)
+            {
+                return Request.CreateResponse(new { response = result, message = "Success" });
+            }
+            else
+            {
+                return Request.CreateResponse(new { response = 0, message = "Failed" });
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddCountry(CountryForm countryForm)
+        {
+            if (ModelState.IsValid)
+            {
+                var country = new COUNTRY();
+                if (countryForm.sno == 0)
+                {
+                    var isExistCountry = country.ValidateLicense(countryForm.Country_Name.ToLower());
+                    if (isExistCountry)
+                    {
+                        return Request.CreateResponse(new { response = 0, message = new List<string> { "Country exists" } });
+                    }
+                    country.Country_Name = countryForm.Country_Name;
+                    country.SNO = countryForm.sno;
+                    try
+                    {
+                        var addedCountry = country.Addcountries(country);
+                        if (addedCountry > 0)
+                        {
+                            var list = new List<string> { addedCountry.ToString(), country.Country_Name };
+                            Auditlog ad = new Auditlog();
+                            for (int i = 0; i < list.Count(); i++)
+                            {
+                                ad.Audit_Type = "Insert";
+                                ad.Columnsname = list[i];
+                                ad.Table_Name = "Country";
+                                ad.Newvalues = list[i];
+                                ad.AuditBy = countryForm.userid.ToString();
+                                ad.Audit_Date = DateTime.Now;
+                                ad.Audit_Time = DateTime.Now;
+
+                                ad.AddAudit(ad);
+                            }
+                            return Request.CreateResponse(new { response = addedCountry, message = new List<string>() });
+                        }
+                        else
+                        {
+                            return Request.CreateResponse(new { response = 0, message = new List<string> { "Failed to add country." } });
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        return Request.CreateResponse(new { response = 0, message = new List<string> { "An error occured on the server." } });
+                    }
+                }
+                else
+                {
+                    if (countryForm.dummy == false)
+                    {
+                        return Request.CreateResponse(new { response = countryForm.dummy, message = new List<string> { "Dummy must be true for country updates." } });
+                    }
+                    else
+                    {
+                        country.Country_Name = countryForm.Country_Name;
+                        country.SNO = countryForm.sno;
+                        var updatedCountry = country.Updatecountries(country);
+
+                        return Request.CreateResponse(new { response = updatedCountry, message = new List<string>() });
+                    }
+                }
+            }
+            else
+            {
+                var errorMessages = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return Request.CreateResponse(new { response = 0, message = errorMessages });
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage DeleteCount(DeleteCountryForm deleteCountryForm)
+        {
+            if (ModelState.IsValid)
+            {
+                if (deleteCountryForm.sno > 0)
+                {
+                    var country = new COUNTRY();
+                    country.SNO = deleteCountryForm.sno;
+                    var isExistSno = country.isExistCountry(deleteCountryForm.sno);
+                    if (isExistSno)
+                    {
+                        var removed = country.Deletecountries(deleteCountryForm.sno);
+                        return Request.CreateResponse(new { response = removed, message = new List<string>() });
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(new { response = 0, message = new List<string> { "Failed. Country does not exist." } });
+                    }
+                }
+                else
+                {
+                    return Request.CreateResponse(new { response = 0, message = new List<string> { "Invalid sno." } });
+                }
+            }
+            else
+            {
+                var errorMessages = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return Request.CreateResponse(new { response = 0, message = errorMessages });
+            }
+        }
+
+
+
+
         // GET: Country
-        COUNTRY cty = new COUNTRY();
+        /*COUNTRY cty = new COUNTRY();
         EMP_DET ed = new EMP_DET();
         private readonly dynamic returnNull = null;
         Auditlog ad = new Auditlog();
@@ -208,7 +327,7 @@ namespace JichangeApi.Controllers
             }
 
             return returnNull;
-        }
-   
+        }*/
+
     }
 }
